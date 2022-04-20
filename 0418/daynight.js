362bc31b23dd28b5161b83c32e1dfbcdae817214
Han <gksdjf80@naver.com>
2022-04-20 11:51:22 +0900

    읽기 완성

--- 0418/daynight.js
+++ 0418/daynight.js
@@ -21,3 +21,11 @@ export function dayNight(mode) {
     day();
   }
 }
+import { yuanToWon, yenToWon } from './exchange/asia.js';
+import dollarToWon from './exchange/dollar.js';
+import euroToWon from './exchange/euro.js';
+// 아래 코드가 수행될 수 있도록 exchange 폴더에서 모듈을 import 하세요.
+console.log(euroToWon(100));
+console.log(dollarToWon(200));
+console.log(yuanToWon(300));
+console.log(yenToWon(400));
